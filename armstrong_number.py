6d144c06bfa63e54836c27974aed923e788cb76d
morfysysdev-ss <morfysysdev.ss@smartdatainc.net>
2023-11-04 21:30:37 +0530

    Practical 2

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,7 +1,10 @@
 number = int(input("Enter a number: "))
 num_str = str(number)
 num_digits = len(num_str)
-total = sum(int(digit) ** num_digits for digit in num_str)
+total = 0
+
+for values in num_str:
+    total = total + int(values) ** num_digits
 
 if total == number:
     print(number, "is an Armstrong number.")
