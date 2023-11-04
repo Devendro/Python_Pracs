1e86c93639bd90ea382dac111d0d0f9e765f065d
morfysysdev-ss <morfysysdev.ss@smartdatainc.net>
2023-11-04 21:20:59 +0530

    Practical 2

--- prime_number_between_0_to_100.py
+++ prime_number_between_0_to_100.py
@@ -1,14 +1,9 @@
-for num in range (0, 101):
-    if num <= 1:
-        continue
-    if num <= 3:
-        print (num, end=" ")
-    if num % 2 == 0 or num % 3 == 0:
-        continue
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
+for i in range (2, 101):
+    prime = True
+    for j in range (2, i):
+        if(i%j == 0):
+            prime = False
             break
-        i += 6
-    else:
-        print (num, end=" ")
+    
+    if(prime == True):
+        print(i)
